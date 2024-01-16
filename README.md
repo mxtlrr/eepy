8a6ec1e168889a023fba273b0b08f3739b941a38
mxtlrr <ruson0914@gmail.com>
2024-01-15 18:05:50 -0600

    finally

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # eepy
-Eepy is an esoteric programming language. It has six opcodes:
+Eepy is an esoteric programming language written in 100%
+i386 assembly. It has three opcodes:
 
 
 |  Character | Action                         |
 | ---------- | ------------------------------ |
-| Z          | Increments instruction pointer |
-| z          | Decreases  instruction pointer |
 | ż (U+017C) | Increments data pointer        |
 | Ž (U+017D) | Decreases  data pointer        |
 | ℤ (U+2124) | Output ASCII of data pointer   |
@@ -13,3 +12,13 @@ Eepy is an esoteric programming language. It has six opcodes:
 The instruction pointer, and the data pointer is a unsigned 8-bit
 integer. Thus making `IP=256` would cause an infinite loop due
 to `IP` looping back to 0.
+
+
+# Programs
+
+## `eepy` in Eepy
+```
+żżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżżℤℤ
+żżżżżżżżżżżℤ
+żżżżżżżżżℤ
+```
--- src/eepy.asm
+++ src/eepy.asm
@@ -1,9 +1,64 @@
+
+
 section .data
 buf times 4096 db 0 ;; 4 mb of data space
 _RMODE equ 0
 
+dataptr db 0
+
 section .text
 global _start
+
+;; We know that ECX is the buffer containing the program
+;; So we can just loop until buffer is equal to 0, then return.
+EepyHandle:
+  ;; make a back up <333
+  mov esi, ecx
+  .Loop:
+    cmp [ecx], byte 0   ;; No more to read!
+    je .Exit
+
+    cmp [ecx], byte 'ż'
+    je .IncreaseDataPTR
+
+    cmp [ecx], byte 'Ž'
+    je .DecreaseDataPTR
+
+    cmp [ecx], byte 'ℤ'
+    je .OutputDataPTR
+
+    inc ecx
+    jmp .Loop
+  .Exit:
+    ret
+
+  .IncreaseDataPTR:
+    mov edi, [dataptr]
+    inc edi
+    mov [dataptr], edi
+    inc ecx
+    jmp .Loop
+
+  .DecreaseDataPTR:
+    mov edi, [dataptr]
+    dec edi
+    mov [dataptr], edi
+    inc ecx
+    jmp .Loop
+
+  .OutputDataPTR:
+    push ecx
+    mov eax, 0x04
+    mov ebx, 0x01   ;; stdout
+    mov ecx, dataptr
+    mov edx, 1      ;; self explanatory
+    int 0x80
+
+    pop ecx   ;; restore current value
+              ;; here we could also do
+              ;; mov ecx, esi, but eh
+    inc ecx
+    jmp .Loop
 
 _start:
   ;; this is such a gross way of doing it
@@ -22,6 +77,9 @@ _start:
   mov ecx, buf
   mov edx, 512
   int 0x80
+
+  ;; buf is already in ecx.
+  call EepyHandle
 
   jmp _exit
   ret
