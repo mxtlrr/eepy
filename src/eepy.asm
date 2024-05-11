081a0f69aab4fed1ec4f1d6e8d8fac16bc0e86f7
mxtlrr <ruson0914@gmail.com>
2024-05-11 07:59:31 +0000

    ?: working on loops -- enable looping and disable looping

--- src/eepy.asm
+++ src/eepy.asm
@@ -4,7 +4,15 @@ section .data
 buf times 4096 db 0 ;; 4 mb of data space
 _RMODE equ 0
 
-dataptr  db 0
+dataptr    db 0
+inst_ptr   db 0  ;; copy of ecx.
+is_looping db 0  ;; 0 -- No (Z not found yet!) 1 -- Yes!
+loop_ct    db 0
+;; >>
+;; 1 - 10
+;; 2 - 20
+;; ...
+;; 0 - 100 for the number after z (end loop)
 
 section .text
 global _start
@@ -28,6 +36,13 @@ EepyHandle:
 
     cmp [ecx], byte '.'   ;; Set DP to 0
     je .SetDPZero
+
+    cmp [ecx], byte 'Z'   ;; Enable looping
+    je .EnableLooping
+
+    cmp [ecx], byte 'z'   ;; Handle the looping
+    je .HandleLooping
+
 
     ;; Not equal, check for others, and if still
     ;; not equal increment and loop
@@ -69,8 +84,58 @@ EepyHandle:
 
   .i2nc:
     inc ecx
+    ;; Increase instruction pointer. This is only really used
+    ;; for loops
+    push ebx
+      mov ebx, [inst_ptr]
+      inc ebx
+      mov [inst_ptr], ebx
+    pop ebx
     jmp .Loop
   
+
+  .EnableLooping:
+    ;; Is it already enabled?
+    push edx
+    mov edx, [is_looping]
+
+    ;; edx != 0 => ???
+    cmp edx, 0
+    jne $     ;; Just hang. WTF are you doing?
+    je  .SetAndRt
+    pop edx
+
+    .SetAndRt:
+      push edx
+        mov edx, [is_looping]
+        inc edx   ;; edx here should be one
+        mov [is_looping], edx
+      pop edx
+      jmp .i2nc
+
+    .DisableLooping:    ;; after we finish looping
+      push edx
+      mov edx, [is_looping]
+      xor edx, edx
+      mov [is_looping], edx
+      jmp .i2nc
+
+    
+    .HandleLooping:
+      ;; How many times do we need to loop?
+      ;; Next char is amount
+      push eax  ;; Store so we don't end up fucking something up
+      push ebx
+        mov eax, [ecx+1]
+        mov ebx, 10
+        mul ebx    ;; Multiply by 10, loop count
+        mov [loop_ct], eax
+      pop ebx
+      pop eax
+      ;; loop_ct has amount of times we need to loop
+
+      ;; Stop looping, we need to return.
+      jmp .DisableLooping
 
 _start:
   ;; Get first argument
