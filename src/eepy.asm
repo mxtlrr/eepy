81c21d2d80e1173d06fa5c9fa748121f8160f4ea
mxtlrr <ruson0914@gmail.com>
2024-05-12 02:37:48 +0000

    feat: basic loops work for n=1. (#2) (#5)

--- src/eepy.asm
+++ src/eepy.asm
@@ -9,10 +9,10 @@ inst_ptr   db 0  ;; copy of ecx.
 is_looping db 0  ;; 0 -- No (Z not found yet!) 1 -- Yes!
 loop_ct    db 0
 ;; >>
-;; 1 - 10
-;; 2 - 20
+;; 1 - 1
+;; 2 - 2
 ;; ...
-;; 0 - 100 for the number after z (end loop)
+;; 0 - 10 for the number after z (end loop)
 
 section .text
 global _start
@@ -87,23 +87,27 @@ EepyHandle:
     ;; Increase instruction pointer. This is only really used
     ;; for loops
     push ebx
-      mov ebx, [inst_ptr]
+      mov ebx, [is_looping]      
+      cmp ebx, 1 ;; Looping... update
+        je .Yess
+        jne .Loop
+
+    .Yess:    ;; Increaseinstruction pointer
+      mov ebx, [is_looping]
       inc ebx
       mov [inst_ptr], ebx
-    pop ebx
-    jmp .Loop
+      pop ebx
+      jmp .Loop ;; Go back
   
 
   .EnableLooping:
     ;; Is it already enabled?
-    push edx
     mov edx, [is_looping]
 
     ;; edx != 0 => ???
     cmp edx, 0
     jne $     ;; Just hang. WTF are you doing?
     je  .SetAndRt
-    pop edx
 
     .SetAndRt:
       push edx
@@ -122,22 +126,36 @@ EepyHandle:
 
     
     .HandleLooping:
+      push edx
+        mov edx, [is_looping]
+        cmp edx, 1
+        jne .DisableLooping ;; Stop looping, don't do anything. 
+      pop edx
       ;; How many times do we need to loop?
       ;; Next char is amount
       push eax  ;; Store so we don't end up fucking something up
       push ebx
         mov eax, [ecx+1]
-        mov ebx, 10
-        mul ebx    ;; Multiply by 10, loop count
         mov [loop_ct], eax
       pop ebx
       pop eax
       ;; loop_ct has amount of times we need to loop
 
-      ;; Stop looping, we need to return.
-      jmp .DisableLooping
+      ;; Delete everything that we did from ecx
+      push edx
+        mov edx, [inst_ptr]
+        sub ecx, edx    ;; ecx - inst_ptr. This will not increase on
+                        ;; the 'z'.
+      pop edx
+
+      ;; Now we can go back to .Loop, and see if it works.
+      jmp .Loop
 
 _start:
+  mov edx, [is_looping]
+  xor edx, edx
+  mov [is_looping], edx
+  
   ;; Get first argument
   lea ecx, [esp+8]
   mov ebx, [ecx]
